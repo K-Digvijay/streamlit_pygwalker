1cb59bd5bc899733ef260a131a62da7cbec56bd3
Digvjay <digvijay.kewale@gmail.com>
2024-09-23 21:29:20 +0530

    screenshot added

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import streamlit as st
 import pygwalker as pyg
-import streamlit.components.v1 as components
 from pygwalker.api.streamlit import StreamlitRenderer
 
 df = pd.read_csv('diamond_data.csv')
